bb386008367a4737d5d3041107742b4c796dcb6b
David Ackerman <david.s.ackerman@gmail.com>
2014-02-08 03:16:55 -0700

    Files can now be read from the object store (cached locally first). They can also be chmoded and chowned.

--- file_system.py
+++ file_system.py
@@ -18,7 +18,7 @@ from fsnode import FSNode
 class FileSystem(LoggingMixIn, Operations):
 	def __init__(self, config):
 		self.config = config
-		self.cache_dir = os.path.realpath(config["cache_dir"])		
+		self.cache_root = os.path.realpath(config["cache_dir"])
 
 		self.swift_connection = SwiftSource(
 			auth_url=config["swift.auth_url"],
@@ -67,8 +67,8 @@ class FileSystem(LoggingMixIn, Operations):
 
 		session.commit()
 
-	def get(self, path):
-		return None
+	def get(self, path, session):
+		return session.query(FSNode).filter(FSNode.path==path.lstrip("/")).first()
 
 
 	def access(self, path, mode):
@@ -76,28 +76,47 @@ class FileSystem(LoggingMixIn, Operations):
 		return self.file_system.access(path, mode)
 
 	def chmod(self, path, mode):
-		return self.object_for_path(path).chmod(mode)
+		session = Session()
+		node = self.get(path, session)
+		node.mode = mode
+		session.add(node)
+		session.commit()
+		# TODO: This has the potential to be quite slow. We may want to send the
+		# operation off to a background process
+		obj = self.swift_connection.get_object(path)
+		obj.set_metadata({ "fs-mode": "%i" % mode })
+		return 0
 
 	def chown(self, path, uid, gid):
-		return self.object_for_path(path).chown(uid, gid)
+		session = Session()
+		node = self.get(path, session)
+		node.uid = uid
+		node.gid = gid
+		session.add(node)
+		session.commit()
+		# TODO: This has the potential to be quite slow. We may want to send the
+		# operation off to a background process
+		obj = self.swift_connection.get_object(path)
+		obj.set_metadata({ "fs-uid": "%i" % uid, "fs-gid": "%i" % gid })
+		return 0
 
 	def create(self, path, mode):
 		return self.file_system.create(path, mode)
 
 	def flush(self, path, fh):
-		return self.object_for_path(path).flush(fh)
+		# TODO: This may be the spot to write modified files back to the object store
+		return 0
 
 	def fsync(self, path, datasync, fh):
 		return self.object_for_path(path).fsync(datasync, fh)
 
 	def getattr(self, path, fh=None):
 		if path == "/":
-			st = os.lstat(self.cache_dir)
+			st = os.lstat(self.cache_root)
 			return dict((key, getattr(st, key)) for key in ('st_atime', 'st_ctime',
 				'st_gid', 'st_mode', 'st_mtime', 'st_nlink', 'st_size', 'st_uid'))
 		else:
-			session = Session()
-			return session.query(FSNode).filter(FSNode.path==path.lstrip("/")).first().attr()
+			return self.get(path, Session()).attr()
 
 	def getxattr(self, path, name, position=0):
 		return ""
@@ -106,7 +125,7 @@ class FileSystem(LoggingMixIn, Operations):
 		return self.file_system.link(target, source)
 
 	def listxattr(self, path):
-		return self.object_for_path(path).listxattr()
+		return None
 
 	def mkdir(self, path, mode):
 		return self.file_system.mkdir(path, mode)
@@ -115,10 +134,22 @@ class FileSystem(LoggingMixIn, Operations):
 		return self.file_system.mknod(path, mode, dev)
 
 	def open(self, path, flags):
-		return self.object_for_path(path).open(flags)
+		# TODO: Do we really need to do anything here?
+		return 0
 
 	def read(self, path, size, offset, fh):
-		return self.object_for_path(path).read(size, offset, fh)
+		if not os.path.exists(self.cache_path(path)):
+			# TODO: start a process to grab this object in the background and return the requested 
+			# data from this function as soon as it's available
+			obj = self.swift_connection.get_object(path)
+			cache_file = open(self.cache_path(path), 'w')
+			cache_file.write(obj.fetch()) # python will convert \n to os.linesep
+			cache_file.close()
+
+		# TODO: Do we need to grab a lock before the read, as in the loopback example?
+		cache_file = open(self.cache_path(path), 'r')
+		cache_file.seek(offset)
+		return cache_file.read(size)
 
 	def readdir(self, path, fh):
 		session = Session()
@@ -128,7 +159,8 @@ class FileSystem(LoggingMixIn, Operations):
 		return self.object_for_path(path).readlink()
 
 	def release(self, path, fh):
-		return self.object_for_path(path).release(fh)
+		# TODO: This may be the spot to write modified files back to the object store
+		return 0
 
 	def rename(self, old, new):
 		return self.file_system.rename(old, new)
@@ -155,11 +187,8 @@ class FileSystem(LoggingMixIn, Operations):
 		return self.object_for_path(path).write(data, offset, fh)
 
 
-
-
-
-	# def cache_path(self, path):
-	# 	return os.path.join(self.cache_root, path.lstrip("/"))
+	def cache_path(self, path):
+		return os.path.join(self.cache_root, path.lstrip("/"))
 
 	# def mknod(self, path, mode, dev):
 	# 	return os.mknod(self.cache_path(path), mode, dev)
@@ -197,6 +226,7 @@ class FileSystem(LoggingMixIn, Operations):
 	def __call__(self, op, path, *args):
 		retval = super(FileSystem, self).__call__(op, path, *args)
 
-		print "called %s (path: %s | args: %s)" % (op, path, args)
+		if op != "get":
+			print "called %s (path: %s | args: %s)" % (op, path, args)
 
 		return retval
